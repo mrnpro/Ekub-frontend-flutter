d33991d18c7a8c898daf243e4c741f4d8d3b587c
mrnpro <yegetanaty03@gmail.com>
2022-06-30 17:04:36 +0300

    login form added

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 const String logoImgPath = 'asset/images/ekublogo.png';
 const kwhite = Colors.white;
+const kprimary = Colors.blue;
+const kspace = SizedBox(
+  height: 10,
+);
--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -7,12 +7,71 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: kwhite,
-      body: SafeArea(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SafeArea(
+            child: SingleChildScrollView(
           child: Column(
-        children: const [Logo()],
-      )),
+            children: [
+              Container(width: size.width / 3, child: const Logo()),
+              Form(
+                  child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: kprimary, width: 1.0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: kprimary, width: 1.0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  kspace,
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "password",
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: kprimary, width: 1.0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide:
+                            const BorderSide(color: kprimary, width: 1.0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  kspace,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                          onPressed: () {}, child: const Text("Register?")),
+                      TextButton(
+                          onPressed: () {},
+                          child: const Text("Forgot Passwod?"))
+                    ],
+                  ),
+                  kspace,
+                ],
+              ))
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
